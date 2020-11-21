37e4c45a24d5008e5101be335b833a4a48d3970b
Lucas de Oliveira <lucasvitalate@gmail.com>
2020-11-21 16:56:44 -0300

    Remove password validation from settings

--- favorites_api/favorites_api/settings.py
+++ favorites_api/favorites_api/settings.py
@@ -74,17 +74,6 @@ WSGI_APPLICATION = "favorites_api.wsgi.application"
 DATABASES = {"default": parse_db_url(env("DATABASE_URL"))}
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
