4fc39a6fb4be5b21bd1c3a3c82e6fbffbf844efd
Lucas de Oliveira <lucasvitalate@gmail.com>
2020-11-24 01:59:13 -0300

    Add favorites app models

---
+++ favorites_api/apps/favorites/models.py
@@ -0,0 +1,40 @@
+from uuid import uuid4
+
+from django.db import models
+
+
+class TimestampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Product(TimestampModel):
+    serializer = "apps.favorites.serializers.ProductSerializer"
+    product_id = models.UUIDField(primary_key=True)
+
+
+class Customer(UUIDModel, TimestampModel):
+    serializer = "apps.favorites.serializers.CustomerSerializer"
+
+    email = models.EmailField(unique=True, db_index=True)
+    name = models.CharField(max_length=128)
+    favorites = models.ManyToManyField(Product, related_name="customer_favorites")
+
+
+class DetailedProduct:
+    def __init__(self, id, price, image, brand, title):
+        self.id = id
+        self.price = price
+        self.image = image
+        self.brand = brand
+        self.title = title
