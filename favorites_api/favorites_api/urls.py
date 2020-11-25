0ae1e63337b1e5189492a7852dc2ffe709397721
Lucas de Oliveira <lucasvitalate@gmail.com>
2020-11-25 01:21:22 -0300

    Use simplejwt as authentication method for API

--- favorites_api/favorites_api/urls.py
+++ favorites_api/favorites_api/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     re_path(r"^v1/", include("apps.favorites.urls", namespace="favorites")),
+    re_path(r"^v1/auth/", TokenObtainPairView.as_view(), name="token_obtain_pair_view"),
+    re_path(r"^v1/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- favorites_api/tests/conftest.py
+++ favorites_api/tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 
 
@@ -19,7 +20,15 @@ def luizalabs_product(id):
     }
 
 
-@pytest.fixture()
-def client_api():
+@pytest.fixture
+def user():
+    user_data = {"username": "admin", "password": "admin"}
+    user = get_user_model().objects.create_user(**user_data)
+    return user
+
+
+@pytest.fixture
+def client_api(user):
     client = APIClient()
+    client.force_authenticate(user=user)
     return client
